b1d9a12abcc5d9647a26c90883627950d188e3e1
li03055 <131619333+li03055@users.noreply.github.com>
2023-05-14 14:52:38 +0300

    Add files via upload

---
+++ ex 1.3.1.py
@@ -0,0 +1,7 @@
+def intersection(list_1, list_2):
+    return list(set(list_1) & set(list_2))
+
+
+print(intersection([1, 2, 3, 4], [8, 3, 9]))
+print(intersection([5, 5, 6, 6, 7, 7], [1, 5, 9, 5, 6]))
+
